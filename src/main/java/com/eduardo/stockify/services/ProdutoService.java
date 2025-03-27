b16bc8bde93525e47ca29234ad20c3eb165debc2
eduardo-toste <duciochetti@gmail.com>
2025-03-27 19:14:53 -0300

    feat: creating product service

---
+++ src/main/java/com/eduardo/stockify/services/ProdutoService.java
@@ -0,0 +1,12 @@
+package com.eduardo.stockify.services;
+
+import com.eduardo.stockify.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repository;
+}
