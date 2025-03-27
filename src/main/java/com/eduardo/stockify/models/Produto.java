c6c149c304273a917eeb83efdfb978c17eda01b5
eduardo-toste <duciochetti@gmail.com>
2025-03-27 17:29:17 -0300

    feat: creating product model

---
+++ src/main/java/com/eduardo/stockify/models/Produto.java
@@ -0,0 +1,31 @@
+package com.eduardo.stockify.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "Produto")
+@Table(name = "produtos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidade;
+
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+    private LocalDateTime dataCadastro;
+
+}
