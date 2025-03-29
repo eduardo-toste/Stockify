6741a84f6624966a09d744c499f3e8d198856d2b
eduardo-toste <duciochetti@gmail.com>
2025-03-29 16:02:03 -0300

    feat: creating usuario model

---
+++ src/main/java/com/eduardo/stockify/models/Usuario.java
@@ -0,0 +1,23 @@
+package com.eduardo.stockify.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+}
