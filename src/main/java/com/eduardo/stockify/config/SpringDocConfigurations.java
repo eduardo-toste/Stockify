03f84d5a1126ef98c3ae32825f14bb8647b6f4dc
eduardo-toste <duciochetti@gmail.com>
2025-03-31 08:50:32 -0300

    doc: creating springdoc configurations

---
+++ src/main/java/com/eduardo/stockify/config/SpringDocConfigurations.java
@@ -0,0 +1,29 @@
+package com.eduardo.stockify.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Stockify API")
+                        .description("API Rest da aplicação Sctockify, para gerenciamento de estoque")
+                        .contact(new Contact()
+                                .name("Time Backend")
+                                .email("backend@stockify.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://stockify/api/licenca")));
+    }
+
+}
