bca6f3ff6a5d09d167b490531e4bbdd25bf98594
eduardo-toste <duciochetti@gmail.com>
2025-03-31 21:27:42 -0300

    feat: creating method to create product

--- src/main/java/com/eduardo/stockify/services/ProdutoService.java
+++ src/main/java/com/eduardo/stockify/services/ProdutoService.java
@@ -1,12 +1,37 @@
 package com.eduardo.stockify.services;
 
+import com.eduardo.stockify.dtos.DadosCadastroProduto;
+import com.eduardo.stockify.dtos.DadosDetalhamentoProduto;
+import com.eduardo.stockify.models.Produto;
 import com.eduardo.stockify.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class ProdutoService {
 
     @Autowired
     private ProdutoRepository repository;
+
+    public DadosDetalhamentoProduto criarProduto(DadosCadastroProduto dados){
+        if(repository.existsByNome(dados.nome())){
+            throw new RuntimeException("Já existe um produto cadastrado com este nome");
+        }
+
+        var produto = new Produto(
+                null,
+                dados.nome(),
+                dados.descricao(),
+                dados.preco(),
+                dados.quantidade(),
+                dados.categoria(),
+                LocalDateTime.now()
+        );
+
+        var produtoSalvo = repository.save(produto);
+
+        return new DadosDetalhamentoProduto(produtoSalvo);
+    }
 }
