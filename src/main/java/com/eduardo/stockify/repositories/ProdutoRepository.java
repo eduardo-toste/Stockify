971fbcf9772e4b03990448ea519f3a54fae892de
eduardo-toste <duciochetti@gmail.com>
2025-03-31 21:27:28 -0300

    feat: creating method to search product

--- src/main/java/com/eduardo/stockify/repositories/ProdutoRepository.java
+++ src/main/java/com/eduardo/stockify/repositories/ProdutoRepository.java
@@ -4,4 +4,7 @@ import com.eduardo.stockify.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    boolean existsByNome(String nome);
+
 }
