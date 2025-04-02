cf7078055fc82374142dbcce8f2070eb2f043fe6
eduardo-toste <duciochetti@gmail.com>
2025-04-02 20:43:02 -0300

    feat: creating product update query

--- src/main/java/com/eduardo/stockify/repositories/ProdutoRepository.java
+++ src/main/java/com/eduardo/stockify/repositories/ProdutoRepository.java
@@ -1,10 +1,21 @@
 package com.eduardo.stockify.repositories;
 
+import com.eduardo.stockify.models.Categoria;
 import com.eduardo.stockify.models.Produto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     boolean existsByNome(String nome);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Produto p SET p.nome = :nome, p.descricao = :descricao, p.preco = :preco, p.quantidade = :quantidade, p.categoria = :categoria WHERE p.id = :id")
+    int atualizarProduto(Long id, String nome, String descricao, double preco, int quantidade, Categoria categoria);
 }
