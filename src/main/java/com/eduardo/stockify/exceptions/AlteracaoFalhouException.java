223fec240d15d5443cf0d90a9b0e5c8f6f7fce55
eduardo-toste <duciochetti@gmail.com>
2025-04-02 20:42:39 -0300

    feat: creating a failed update exception

---
+++ src/main/java/com/eduardo/stockify/exceptions/AlteracaoFalhouException.java
@@ -0,0 +1,9 @@
+package com.eduardo.stockify.exceptions;
+
+public class AlteracaoFalhouException extends RuntimeException {
+
+    public AlteracaoFalhouException(String message) {
+        super(message);
+    }
+
+}
