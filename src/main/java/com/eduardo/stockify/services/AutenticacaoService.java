0d99aa60237c63051e40d3bf2c504c6c488c84f3
eduardo-toste <duciochetti@gmail.com>
2025-04-02 22:58:40 -0300

    feat: creating register method

--- src/main/java/com/eduardo/stockify/services/AutenticacaoService.java
+++ src/main/java/com/eduardo/stockify/services/AutenticacaoService.java
@@ -1,10 +1,14 @@
 package com.eduardo.stockify.services;
 
+import com.eduardo.stockify.dtos.AutenticacaoRequest;
+import com.eduardo.stockify.exceptions.UsuarioExistenteException;
+import com.eduardo.stockify.models.Usuario;
 import com.eduardo.stockify.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +17,23 @@ public class AutenticacaoService implements UserDetailsService {
     @Autowired
     private UsuarioRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByUsername(username);
+    }
+
+    public void registrar(AutenticacaoRequest dados) {
+        var usuario = repository.findByUsername(dados.username());
+
+        if(usuario != null){
+            throw new UsuarioExistenteException("Este usuário já foi cadastrado!");
+        }
+
+        var senhaCriptografada = passwordEncoder.encode(dados.password());
+
+        repository.save(new Usuario(null, dados.username(), senhaCriptografada));
     }
 }
