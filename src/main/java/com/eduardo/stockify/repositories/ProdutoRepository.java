38802aeca92cfca4795fb35959fac01c2269bb2c
eduardo-toste <duciochetti@gmail.com>
2025-04-03 16:27:22 -0300

    feat: creating quantity update methods

--- src/main/java/com/eduardo/stockify/repositories/ProdutoRepository.java
+++ src/main/java/com/eduardo/stockify/repositories/ProdutoRepository.java
@@ -2,6 +2,7 @@ package com.eduardo.stockify.repositories;
 
 import com.eduardo.stockify.models.enums.Categoria;
 import com.eduardo.stockify.models.Produto;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +16,12 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     @Transactional
     @Query("UPDATE Produto p SET p.nome = :nome, p.descricao = :descricao, p.preco = :preco, p.quantidade = :quantidade, p.categoria = :categoria WHERE p.id = :id")
     int atualizarProduto(Long id, String nome, String descricao, double preco, int quantidade, Categoria categoria);
+
+    @Query("SELECT p.quantidade FROM Produto p WHERE p.id = :produtoId")
+    int verificarQuantidade(Long produtoId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Produto p SET p.quantidade = :quantidadeAtualizada WHERE p.id = :produtoId")
+    int alterarQuantidade(Long produtoId, int quantidadeAtualizada);
 }
