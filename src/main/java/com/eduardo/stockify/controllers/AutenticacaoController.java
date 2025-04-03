6ace0467734f17d17dcdf4e64a595bafc277698c
eduardo-toste <duciochetti@gmail.com>
2025-04-02 22:58:35 -0300

    feat: creating register endpoint

--- src/main/java/com/eduardo/stockify/controllers/AutenticacaoController.java
+++ src/main/java/com/eduardo/stockify/controllers/AutenticacaoController.java
@@ -3,9 +3,11 @@ package com.eduardo.stockify.controllers;
 import com.eduardo.stockify.dtos.AutenticacaoRequest;
 import com.eduardo.stockify.dtos.TokenResponse;
 import com.eduardo.stockify.models.Usuario;
+import com.eduardo.stockify.services.AutenticacaoService;
 import com.eduardo.stockify.services.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,6 +26,9 @@ public class AutenticacaoController {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private AutenticacaoService service;
+
     @PostMapping("/login")
     public ResponseEntity efetuarLogin(@RequestBody @Valid AutenticacaoRequest dados){
         var authenticationToken = new UsernamePasswordAuthenticationToken(dados.username(), dados.password());
@@ -32,6 +37,13 @@ public class AutenticacaoController {
         var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
 
         return ResponseEntity.ok(new TokenResponse(tokenJWT));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity registrar(@RequestBody @Valid AutenticacaoRequest dados){
+        service.registrar(dados);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
