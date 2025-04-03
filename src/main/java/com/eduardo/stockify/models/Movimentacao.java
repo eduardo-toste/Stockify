b5b2914d114880a97948d3b62a2bf014b54ba464
eduardo-toste <duciochetti@gmail.com>
2025-04-03 13:18:47 -0300

    feat: creating movement entity

---
+++ src/main/java/com/eduardo/stockify/models/Movimentacao.java
@@ -0,0 +1,33 @@
+package com.eduardo.stockify.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "Movimentacao")
+@Table(name = "movimentacoes")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Movimentacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Produto produto;
+
+    @Enumerated(EnumType.STRING)
+    private TipoMovimentacao tipoMovimentacao;
+
+    private int quantidade;
+    private LocalDateTime dataMovimentacao;
+
+}
