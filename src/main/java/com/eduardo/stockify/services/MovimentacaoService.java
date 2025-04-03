095977ba159bc018806fb16829702f4a4ac43863
eduardo-toste <duciochetti@gmail.com>
2025-04-03 16:28:24 -0300

    feat: creating movement service

---
+++ src/main/java/com/eduardo/stockify/services/MovimentacaoService.java
@@ -0,0 +1,58 @@
+package com.eduardo.stockify.services;
+
+import com.eduardo.stockify.dtos.MovimentacaoRequest;
+import com.eduardo.stockify.dtos.MovimentacaoResponse;
+import com.eduardo.stockify.exceptions.AlteracaoFalhouException;
+import com.eduardo.stockify.models.Movimentacao;
+import com.eduardo.stockify.repositories.MovimentacaoRepository;
+import com.eduardo.stockify.repositories.ProdutoRepository;
+import com.eduardo.stockify.services.validations.ValidacaoEspecifica;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class MovimentacaoService {
+
+    @Autowired
+    private MovimentacaoRepository movimentacaoRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private List<ValidacaoEspecifica> validacaoEspecifica;
+
+    public MovimentacaoResponse movimentacao(MovimentacaoRequest dados) {
+        validacaoEspecifica.forEach(v -> v.validar(dados.produtoId()));
+        var quantidadeAtual = produtoRepository.verificarQuantidade(dados.produtoId());
+        int quantidadeAtualizada;
+        int linhasAfetadas;
+
+        if(dados.tipo().equals("ENTRADA")){
+            quantidadeAtualizada = quantidadeAtual + dados.quantidade();
+            linhasAfetadas = produtoRepository.alterarQuantidade(dados.produtoId(), quantidadeAtualizada);
+        } else {
+            quantidadeAtualizada = quantidadeAtual - dados.quantidade();
+            linhasAfetadas = produtoRepository.alterarQuantidade(dados.produtoId(), quantidadeAtualizada);
+        }
+
+        if(linhasAfetadas == 0){
+            throw new AlteracaoFalhouException("A movimentação do produto falhou!");
+        }
+
+        var movimentacao = new Movimentacao(
+                null,
+                dados.produtoId(),
+                dados.tipo(),
+                dados.quantidade(),
+                LocalDateTime.now()
+        );
+
+        var movimentacaoSalva = movimentacaoRepository.save(movimentacao);
+
+        return new MovimentacaoResponse(movimentacaoSalva);
+    }
+}
