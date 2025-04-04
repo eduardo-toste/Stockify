6295c87298a6f71425a879442685dd29a7223d76
eduardo-toste <duciochetti@gmail.com>
2025-04-04 18:22:31 -0300

    refactor: returning name

--- src/main/java/com/eduardo/stockify/services/validations/impl/ValidaProdutoExistente.java
+++ src/main/java/com/eduardo/stockify/services/validations/impl/ValidaProdutoExistente.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ValidaRegistroExistente implements ValidacaoGeral {
+public class ValidaProdutoExistente implements ValidacaoGeral {
 
     @Autowired
     ProdutoRepository repository;
