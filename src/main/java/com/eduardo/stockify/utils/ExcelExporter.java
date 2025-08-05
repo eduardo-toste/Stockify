aff527529b255dcc1ce5918195c295a3afd652ce
eduardo-toste <duciochetti@gmail.com>
2025-08-05 08:46:41 -0300

    refactor: create method to send files to s3 and update export method to private

--- src/main/java/com/eduardo/stockify/utils/ExcelExporter.java
+++ src/main/java/com/eduardo/stockify/utils/ExcelExporter.java
@@ -1,15 +1,40 @@
 package com.eduardo.stockify.utils;
 
+import com.eduardo.stockify.aws.s3.S3Service;
+import lombok.RequiredArgsConstructor;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.ResponseEntity;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.UUID;
 
+@RequiredArgsConstructor
 public abstract class ExcelExporter<T> {
 
-    public byte[] export(String sheetName) throws IOException {
+    private final S3Service s3Service;
+
+    public Map<String, String> enviarParaS3(String sheetName) throws IOException {
+        byte[] excelBytes = export(sheetName);
+
+        String fileName = "exportacoes/" + sheetName.toLowerCase() + "-" + UUID.randomUUID() + ".xlsx";
+
+        s3Service.uploadFile(
+                excelBytes,
+                fileName,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        );
+
+        String presignedUrl = s3Service.generatePresignerUrl(fileName, Duration.ofMinutes(30));
+
+        return Map.of("url", presignedUrl);
+    }
+
+    private byte[] export(String sheetName) throws IOException {
         try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             Sheet sheet = workbook.createSheet(sheetName);
             createHeader(sheet);
