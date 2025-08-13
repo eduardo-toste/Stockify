71be570509f6913423136da43b7af98c20aa3c76
eduardo-toste <duciochetti@gmail.com>
2025-08-13 20:32:36 -0300

    refactor: fixing user repository returns

--- src/main/java/com/eduardo/stockify/repositories/UsuarioRepository.java
+++ src/main/java/com/eduardo/stockify/repositories/UsuarioRepository.java
@@ -4,8 +4,12 @@ import com.eduardo.stockify.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    UserDetails findByUsername(String username);
+    Optional<Usuario> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 
 }
--- src/main/java/com/eduardo/stockify/services/AutenticacaoService.java
+++ src/main/java/com/eduardo/stockify/services/AutenticacaoService.java
@@ -4,6 +4,7 @@ import com.eduardo.stockify.dtos.AutenticacaoRequest;
 import com.eduardo.stockify.exceptions.UsuarioExistenteException;
 import com.eduardo.stockify.models.Usuario;
 import com.eduardo.stockify.repositories.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,23 +13,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AutenticacaoService implements UserDetailsService {
 
-    @Autowired
-    private UsuarioRepository repository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final UsuarioRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByUsername(username);
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado no sistema"));
     }
 
     public void registrar(AutenticacaoRequest dados) {
-        var usuario = repository.findByUsername(dados.username());
-
-        if(usuario != null){
+        if(repository.existsByUsername(dados.username())) {
             throw new UsuarioExistenteException("Este usuário já foi cadastrado!");
         }
 
