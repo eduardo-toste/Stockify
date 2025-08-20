202622eaf536b61ad61d9549fc58fa549e68ed64
eduardo-toste <duciochetti@gmail.com>
2025-08-19 23:29:14 -0300

    feat: implementing more methods

--- src/test/java/com/eduardo/stockify/utils/ProdutoTestUtils.java
+++ src/test/java/com/eduardo/stockify/utils/ProdutoTestUtils.java
@@ -2,8 +2,12 @@ package com.eduardo.stockify.utils;
 
 import com.eduardo.stockify.models.Produto;
 import com.eduardo.stockify.models.enums.Categoria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ProdutoTestUtils {
 
@@ -15,4 +19,20 @@ public class ProdutoTestUtils {
         return criarProduto(1L, "Produto Teste");
     }
 
+    public static List<Produto> criarListaProdutosPadrao() {
+        return List.of(
+                criarProduto(1L, "Produto A"),
+                criarProduto(2L, "Produto B"),
+                criarProduto(3L, "Produto C")
+        );
+    }
+
+    public static Page<Produto> criarPaginaProdutosPadrao(Pageable pageable) {
+        List<Produto> lista = criarListaProdutosPadrao();
+        return new PageImpl<>(lista, pageable, lista.size());
+    }
+
+    public static Page<Produto> criarPaginaPersonalizada(List<Produto> produtos, Pageable pageable) {
+        return new PageImpl<>(produtos, pageable, produtos.size());
+    }
 }
